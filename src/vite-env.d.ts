a5436fc9a14ebc470b91c1251fa05520f11af365
diegomatiasrossi <diegomatiasrossi@gmail.com>
2026-06-24 22:30:38 -0300

    fix(types): add VITE_STRIPE_STUDIO_PRICE_ID to ImportMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   readonly VITE_STRIPE_PRICE_SOLO_PRO_MONTHLY: string
   readonly VITE_STRIPE_PRICE_SOLO_PRO_YEARLY: string
   readonly VITE_STRIPE_PRICE_STUDIO_MONTHLY: string
+  readonly VITE_STRIPE_STUDIO_PRICE_ID: string
   readonly VITE_STRIPE_PRICE_STUDIO_YEARLY: string
 }
 
